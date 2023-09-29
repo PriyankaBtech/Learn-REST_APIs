40350a2b55cb91c3ce8a8360b40ddfbb6ec9557c
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-09-29 18:16:57 +0530

    connect to database

--- IDEA_App/server.js
+++ IDEA_App/server.js
@@ -1,7 +1,24 @@
 const express = require('express')
 const serverConfig = require('./configs/server.config')
+const mongoose = require('mongoose')
+const dbConfig = require('./configs/db.config')
 
 const app = express()
+
+// Logic to connect to mongodb and create an ADMIN user
+// need to have mongodb up and running in local machine
+
+mongoose.connect(dbConfig.DB_URL)
+const db = mongoose.connection
+
+db.on("error", () => {
+    console.log("Error while connecting to database")
+})
+
+db.once("open", () => {
+    console.log("Database is connected")
+})
+
 
 app.listen(serverConfig.PORT, () => {
     console.log(`server started on port number: ${serverConfig.PORT}`)
